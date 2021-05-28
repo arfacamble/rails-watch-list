413400142de8b8b301b337efa3db6189c267e171
Arthur Campbell <arfa.camble@gmail.com>
2021-05-28 14:26:40 +0100

    seed fiddle

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,29 @@ require 'faker'
 puts "---------------------"
 puts "destroying everything"
 Bookmark.destroy_all
-# Movie.destroy_all
+Movie.destroy_all
 List.destroy_all
 
-# puts "-----------------------"
-# puts "creating films from api"
+puts "-----------------------"
+puts "creating films from api"
 
 # number_of_pages = JSON.parse(open('http://tmdb.lewagon.com/movie/top_rated').read)['total_pages']
-# page = 1
-# number_of_pages.times do
-#   puts "Page #{page} of films loading"
-#   url = "http://tmdb.lewagon.com/movie/top_rated?page=#{page}"
-#   films = JSON.parse(URI.open(url).read)['results']
-#   films.each do |f|
-#     og_title = f['original_title']
-#     eng_title = f['title']
-#     overview = f['overview']
-#     rating = f['vote_average']
-#     poster_url = "https://image.tmdb.org/t/p/w500#{f['backdrop_path']}"
-#     title = (og_title == eng_title ? eng_title : "#{og_title} (#{eng_title})")
-#     Movie.create(title: title, overview: overview, poster_url: poster_url, rating:rating)
-#   end
-#   page += 1
-# end
+page = 1
+416.times do
+  puts "Page #{page} of films loading"
+  url = "http://tmdb.lewagon.com/movie/top_rated?page=#{page}"
+  films = JSON.parse(URI.open(url).read)['results']
+  films.each do |f|
+    og_title = f['original_title']
+    eng_title = f['title']
+    overview = f['overview']
+    rating = f['vote_average']
+    poster_url = "https://image.tmdb.org/t/p/w500#{f['backdrop_path']}"
+    title = (og_title == eng_title ? eng_title : "#{og_title} (#{eng_title})")
+    Movie.create(title: title, overview: overview, poster_url: poster_url, rating:rating)
+  end
+  page += 1
+end
 
 puts "--------------"
 puts "creating lists"
