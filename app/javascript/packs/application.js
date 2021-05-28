7ca61e9392eca0b9bfefe6749cc832178784f502
Arthur Campbell <arfa.camble@gmail.com>
2021-05-28 14:13:27 +0100

    select2 for movie dropdown in bookmark creation. said form integrated into list show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,10 @@ import 'bootstrap'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener("turbolinks:load", function() {
+  console.log('init select2')
+  initSelect2();
+});
